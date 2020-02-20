e3ff46c748a474965ec2e228b78cde6d9eadf647
TinaDelli <cecurrier92@gmail.com>
2020-02-20 13:32:39 -0500

    can add items to room

--- src/room.py
+++ src/room.py
@@ -1,5 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 
 class Room:
     def __init__(self, name, description):
@@ -9,8 +10,9 @@ class Room:
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = []
     def __str__(self):
-        return (f'You are now in {self.name}. {self.description}. From here you have a choice:')
+        return (f'You are now in {self.name}. {self.description}. There are {len(self.items)} items in this room. These are {[el.name for el in self.items]}.  From here you have a choice:')
     def get_room_in_direction(self, direction):
         if hasattr(self, f"{direction}_to"):
             return getattr(self, f"{direction}_to")
@@ -28,3 +30,15 @@ class Room:
         return rooms
     def get_rooms_string(self):
         return f"You can go {', '.join(self.get_rooms())}"
+    def get_items(self):
+        item_list = []
+        for el in self.items:
+            item_list.append(f'{el.name}')
+        return item_list
+    def get_items_string(self):
+        return f"{', '.join(self.get_items())}"
+    def remove_item(self, name_match):
+        for el in self.items:
+            if el.name == name_match:
+                self.items.remove(el)
+   
